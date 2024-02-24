b4889b6780cad0d44ef85c25ef75865478f9d167
RohitMalviya <rohitbmalviya@gmail.com>
2024-02-24 19:48:32 +0530

    multer add for file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (request, file, cd) {
+        cd(null, "./public/temp");
+    },
+    filename: function (request, file, cd) {
+        cb(null, file.originalname);
+    },
+});
+
+export const upload = multer({ storage });
